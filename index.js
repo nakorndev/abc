a0582291f927dae694097d4ee9f987956688cb38
นายเอคนเดิม <nakorndev@gmail.com>
2024-11-04 13:11:43 +0700

    fix typo

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.get('/admin/manage-user/:email/permission', (req, res) => {
         <input class="mr-2" type="checkbox" name="permission" ${permission ? 'checked' : ''} />
         Allow user to create article
       </label>
-      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Save</button>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Submit</button>
     </form>
   `);
 });
