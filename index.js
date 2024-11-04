3cdddeb2a4bde5f074dadc4c531cf57975fc2d63
Nakorn Sinpadung <nakorndev@gmail.com>
2024-11-04 12:02:19 +0700

    add create article route

--- index.js
+++ index.js
@@ -41,6 +41,20 @@ app.post('/signup', (req, res) => {
   }
 });
 
+// In-memory storage for articles
+const ARTICLES = [];
+
+// Handle create article
+app.post('/articles', (req, res) => {
+  const { title, body } = req.body;
+  if (title && body) {
+    ARTICLES.push({ title, body });
+    res.send('Article created successfully!');
+  } else {
+    res.send('Title and body are required');
+  }
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
